be37cd3fe4abdbbb963b9c3aef5eb73a3fb89341
万志鹏 <1151333166@qq.com>
2021-01-22 11:26:33 +0800

    秒处理成时分秒

--- tools.js
+++ tools.js
@@ -5,12 +5,12 @@ function extend() {
   let i = 0;
   const length = arguments.length;
   if (Object.prototype.toString.call(arguments[0]) === "[object Boolean]") {
-    deep = arguments[0]; 
+    deep = arguments[0];
     i++;
   }
 
   for (; i < length; i++) {
-    var obj = arguments[i]; 
+    var obj = arguments[i];
     merge(obj);
   }
 
@@ -32,23 +32,63 @@ function extend() {
   return extended;
 }
 
-const obj1 = {
-  a:'1',
-  b:true,
-  c:[1,2,3],
-  f:{
-    a:1,
-    b:2
-  },
-};
-const obj2 = {
-  a:'456',
-  f:{
-    d:'123'
-  },
-  g:123
+// const obj1 = {
+//   a:'1',
+//   b:true,
+//   c:[1,2,3],
+//   f:{
+//     a:1,
+//     b:2
+//   },
+// };
+// const obj2 = {
+//   a:'456',
+//   f:{
+//     d:'123'
+//   },
+//   g:123
+// }
+// const newobj = extend(obj1,obj2)
+// console.log(newobj);
+// const newobj2 = extend(true,obj1,obj2)
+// console.log(newobj2);
+/**
+ * 
+ * @param {*} s 秒变成时分秒
+ */
+const secondsFormat = (s) => {
+  let hour = Math.floor(s / 3600);
+  let minute = Math.floor((s - hour * 3600) / 60);
+  let second = Math.floor(s - hour * 3600 - minute * 60);
+  if (hour) {
+    if (minute < 10) {
+      if (second < 10) {
+        return hour + ":0" + minute + ":0" + second;
+      } else {
+        return hour + ":0" + minute + ":" + second;
+      }
+    } else {
+      if (second < 10) {
+        return hour + ":" + minute + ":0" + second;
+      } else {
+        return hour + ":" + minute + ":" + second;
+      }
+    }
+  } else {
+    if (minute < 10) {
+      if (second < 10) {
+        return "00:0" + minute + ":0" + second;
+      } else {
+        return "00:0" + minute + ":" + second;
+      }
+    } else {
+      if (second < 10) {
+        return "00:" + minute + ":0" + second;
+      } else {
+        return "00:" + minute + ":" + second;
+      }
+    }
+  }
 }
-const newobj = extend(obj1,obj2)
-console.log(newobj);
-const newobj2 = extend(true,obj1,obj2)
-console.log(newobj2);
+const a = secondsFormat(1000)
+console.log(a);
